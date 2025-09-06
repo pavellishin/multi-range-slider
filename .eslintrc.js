96883a8c6824d17778fb7b3a0fbe525ba0e7e0a5
Pavel Lishin <pavel.lishin@gmail.com>
2025-09-06 19:31:34 -0400

    minor cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,22 +21,12 @@ module.exports = {
     // Additional helpful rules
     'no-implicit-globals': 'error', // Disallow declarations in global scope
     'prefer-const': 'warn',         // Suggest using const when variables are never reassigned
-    'no-var': 'warn',              // Require let or const instead of var
+    'no-var': 'warn',               // Require let or const instead of var
     
     // Webpack/module related
     'import/no-unresolved': 'off',  // Turn off if you're not using eslint-plugin-import
   },
-  globals: {
-    // Define any global variables that are available in your project
-    // Examples:
-    // jQuery: 'readonly',
-    // $: 'readonly',
-    // process: 'readonly',  // if using webpack's DefinePlugin
-  },
   ignorePatterns: [
     'node_modules/',
-    'dist/',
-    'build/',
-    '*.min.js',
   ],
 };
